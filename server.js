09e08e71fbdfc1f55b3089463103d98cdb63ff32
Sela Privette <selaprivette@yahoo.com>
2022-06-24 18:02:53 -0400

    heroku

--- server.js
+++ server.js
@@ -19,24 +19,30 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/note
 //READ FROM DB.JSON FILE
 
 app.get("/api/notes", (req, res) => {
-    console.log('retrieving notes');
-    res.json(readJSON())
+    // console.log('retrieving notes');
+    res.json(loadNotes())
 }); 
 
 //adding the DELETE /api/notes route for the db.json file contents
-// app.delete("/api/notes/:id", (req, res) => {
-//     deleteNote(req.params.id)
-//     res.send();
-// });
+app.delete("/api/notes/:id", (req, res) => {
+//    console.log(req.params.id);
+   deleteNote(req.params.id);
+    res.send();
+});
 
 // adding the POST /api/notes route for the db.json file contents
 app.post("/api/notes", (req, res) => {
-    console.log(req.body);
-	const newNote = req.body;
+	//read the request body in as a JSON note
+    const newNote = req.body;
+	//add an id to the JSON note
     newNote.id = new Date().valueOf();
-	const notes = readJSON();
+	//read my old notes into an array
+    const notes = loadNotes();
+	//add my new note to the array
     notes.push(newNote);
+	//save the old notes, plus the new note, to the db.json file
     saveNotes(notes);
+	//send the new note back to the caller
     res.send(newNote);
 });
 
@@ -46,7 +52,7 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.htm
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
 );
-const readJSON = () => {
+const loadNotes = () => {
     let fileContents = fs.readFileSync("./db/db.json", "utf8")
     console.log(fileContents);
     if (fileContents.length != 0) {
@@ -55,6 +61,15 @@ const readJSON = () => {
         return []
      };
 };
+const deleteNote = (id) => {
+    const notes = loadNotes();
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id == id) {
+            notes.splice(i, 1);
+        }
+    }
+    saveNotes(notes);
+}
 const saveNotes = (notes) => {
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
         if (err) console.log(err);
